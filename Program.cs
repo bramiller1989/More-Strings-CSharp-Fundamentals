446c5b8b928fa66681026068f667fe7cad990f98
bramiller1989 <bramiller1989@gmail.com>
2021-01-21 19:08:40 -0600

    add various string method examples

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace MoreStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fullName = "John Smith ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("John", "Johnathan"));
+            
+            
         }
     }
 }
